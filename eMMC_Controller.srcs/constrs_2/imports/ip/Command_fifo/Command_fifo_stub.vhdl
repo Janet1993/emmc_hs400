110248db5d2090b5b4ed0f92c6feb64ddf7bee07
Janet1993 <94812041+Janet1993@users.noreply.github.com>
2026-07-28 17:11:28 +0800

    Add files via upload

---
+++ eMMC_Controller.srcs/constrs_2/imports/ip/Command_fifo/Command_fifo_stub.vhdl
@@ -0,0 +1,37 @@
+-- Copyright 1986-2015 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2015.4 (win64) Build 1412921 Wed Nov 18 09:43:45 MST 2015
+-- Date        : Tue Oct 18 15:38:16 2016
+-- Host        : WuLizhou-PC running 64-bit Service Pack 1  (build 7601)
+-- Command     : write_vhdl -force -mode synth_stub
+--               D:/VivadoProjects/eMMC_Controller_4.6.1.xpr/eMMC_Controller/eMMC_Controller.srcs/sources_1/ip/Command_fifo/Command_fifo_stub.vhdl
+-- Design      : Command_fifo
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg484-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Command_fifo is
+  Port ( 
+    clk : in STD_LOGIC;
+    srst : in STD_LOGIC;
+    din : in STD_LOGIC_VECTOR ( 63 downto 0 );
+    wr_en : in STD_LOGIC;
+    rd_en : in STD_LOGIC;
+    dout : out STD_LOGIC_VECTOR ( 63 downto 0 );
+    full : out STD_LOGIC;
+    empty : out STD_LOGIC
+  );
+
+end Command_fifo;
+
+architecture stub of Command_fifo is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk,srst,din[63:0],wr_en,rd_en,dout[63:0],full,empty";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "fifo_generator_v13_0_1,Vivado 2015.4";
+begin
+end;
